853d1a7a1e0fa978cea62cf0b11c8c4ad2f409d2
DoronAfriat <doron.afriat2001@gmail.com>
2024-09-28 21:08:22 +0300

    Update assembler.c

--- assembler.c
+++ assembler.c
@@ -7,19 +7,18 @@
 
 
 
-
+/*first process to macro*/
 void processMacrosFromFile(char *filename) { 
     char *output_filename = "ps.am"; 
     MacroList *List_head = NULL; 
     MacroNode *Macro_head = NULL; 
-    
     readFileAndInsertMacros(&List_head, &Macro_head, filename); 
     add_to_file(List_head, filename, output_filename); 
     freeMacroList(List_head);
     freeNodeList(Macro_head);
 
 }
-
+/*second process to binar and octal*/
 void processbinarFromFile(char *filename) { 
     WordTable *wordList = NULL;
     Label *labelList = NULL;
@@ -43,32 +42,33 @@ int main(int argc, char *argv[]) {
     char *output_filename = "ps.am";
     int errorCheck = 0;
     int errorCheck2 = 0;
-
+    /*check for exist file*/
     if  (argc < 2)
     {
         printf("Usage: %s <input_file>\n", argv[0]);
         return 1; 
     } 
 
-    filename = argv[1];  
+    filename = argv[1]; 
+    /*check if error for start first process*/ 
     errorCheck = checkFile1ForErrors(filename);
     if (errorCheck == 0 )
     {   
         printf("%s\n", "Starting preassembler");
-        processMacrosFromFile(filename); 
+        processMacrosFromFile(filename);
+        /*check if error for start second process*/  
         errorCheck2 = checkFile2ForErrors(output_filename);
         if (errorCheck2 == 0) {
             printf("%s\n", "Starting binary processing");
-
             processbinarFromFile(output_filename); 
         } 
         else 
-        {
+        {   /*if find error*/
             printf("Error in file '%s'\n", output_filename );
             printf("%s\n", "Does not continue the process");
         }
     } else
-    {
+    { /*if find error*/
         printf("Error in file '%s'\n", filename);
         printf("%s\n", "Does not continue the process");
     }
